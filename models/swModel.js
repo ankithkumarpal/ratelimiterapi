f23c146a58e87e014a21272d14eb1f1e4750110d
ankithkumarpal <ankithpal721@gmail.com>
2024-05-17 17:31:45 +0530

    addexpires

--- models/swModel.js
+++ models/swModel.js
@@ -1,10 +1,11 @@
+const DEFAULT_EXPIRES = 10;
 const mongoose = require('mongoose');
 
 const RequestSchema = new mongoose.Schema({
     timestamp: {
         type: Date,
         default: Date.now,
-        expires: process.env.WINDOW_SIZE / 1000 // TTL index expiration time in seconds
+        expires: (process.env.WINDOW_SIZE ? process.env.WINDOW_SIZE / 1000 : DEFAULT_EXPIRES)
     }
 });
 
