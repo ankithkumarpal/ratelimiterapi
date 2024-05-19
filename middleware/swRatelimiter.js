52a04ca44e01ff3ef11654196a1c2de39d82006b
ankithkumarpal <ankithpal721@gmail.com>
2024-05-19 15:26:14 +0530

    ts5489

--- middleware/swRatelimiter.js
+++ middleware/swRatelimiter.js
@@ -10,17 +10,18 @@ const allowRequest = async (req, res, next) => {
         const count = await SlidingWindow.countDocuments({ timestamp: { $gte: cutoffTime } });
         
         res.status(200).json({message : count , error : false , timetowait : "returng count"})
-        const newRequest = new SlidingWindow({
-            timestamp: now
-        })
+        // return ;
+        // const newRequest = new SlidingWindow({
+        //     timestamp: now
+        // })
 
-        await newRequest.save();
+        // await newRequest.save();
 
-        if (count < 5) {
-            next();
-        } else {
-            res.status(200).json({ message : 'Rate limit exceeded' , timetowait : 10000 , error : true});
-        }
+        // if (count < 5) {
+        //     next();
+        // } else {
+        //     res.status(200).json({ message : 'Rate limit exceeded' , timetowait : 10000 , error : true});
+        // }
     } catch (error) {
         res.status(500).json({ error: error , message : "In catch statement" });
     }
